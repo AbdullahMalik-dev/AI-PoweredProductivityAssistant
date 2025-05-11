b16eb193d710d199c67754be987bd3634c7e2941
AbdullahMalik-dev <abdullah.veyron@gmail.com>
2025-05-11 18:04:00 +0400

    notficationModel.py

--- notficationModel.py
+++ notficationModel.py
@@ -1,13 +1,11 @@
 from notifypy import Notify
 
 def PushNotification(noti: str) -> None:
-
     notification = Notify()
     notification.application_name = " "
     notification.title = "Hey!"
     notification.message = noti
     notification.icon = ""
     notification.send()
-
-
+    
 PushNotification("Are you still there?")
